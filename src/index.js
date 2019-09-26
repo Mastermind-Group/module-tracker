9a734052dd3a20bcfb14c48cd1e349e9dbb30300
brellin <cekakid@gmail.com>
2019-09-25 21:10:22 -0500

    mui index src

--- src/index.js
+++ src/index.js
@@ -7,22 +7,13 @@ import thunk from 'redux-thunk'
 import rootReducer from './Reducers/rootReducer'
 import logger from 'redux-logger'
 import App from './App'
-import JssProvider from 'react-jss/lib/JssProvider'
-import { createGenerateClassName } from '@material-ui/core/styles'
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger))
-
-const generateClassName = createGenerateClassName({
-    dangerouslyUseGlobalCSS: false,
-    productionPrefix: 'c',
-})
 
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-            <JssProvider generateClassName={generateClassName}>
-                <App />
-            </JssProvider>
+            <App />
         </Router>
     </Provider>,
     document.getElementById('root')
