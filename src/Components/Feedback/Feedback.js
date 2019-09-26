6f17a7c997b424191c8ef9dd8da56f4bf326d154
brellin <cekakid@gmail.com>
2019-09-26 12:38:34 -0500

    feedback fix

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -1,17 +1,11 @@
 import React from 'react'
 
-import { FeedbackContainer } from './styles'
-
-const Feedback = () => {
-
-    return (
-
-        <FeedbackContainer>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe3peoQdeMe7_tP88PUSR5T5Plcg9IUVltXgg3nEYYczSATzg/viewform?embedded=true" width="640" height="436" frameborder="0" marginheight="0" marginwidth="0" title='Feedback Form'>Loading…</iframe>
-        </FeedbackContainer>
-
-    )
-
-}
-
-export default Feedback
+export default _ => <iframe
+    src="https://docs.google.com/forms/d/e/1FAIpQLSe3peoQdeMe7_tP88PUSR5T5Plcg9IUVltXgg3nEYYczSATzg/viewform?embedded=true"
+    width="640"
+    height="450"
+    frameborder="0"
+    marginheight="0"
+    marginwidth="0"
+    title='Feedback Form'
+>Loading…</iframe>
